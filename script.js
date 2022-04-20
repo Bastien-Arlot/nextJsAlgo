cf597d8912f02289891623fa6aa298fbe3a1decd
Bastien Arlot <arlot.b@gmail.com>
2022-04-20 15:12:04 +0200

    sort bubble

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ fs.readFile(fileName, 'utf8', (error, data) => {
         return ;
     }
     console.log(data);
+    const arr = data.split(" ").map(num => Number(num));
+    const bubbleSort = (data) => {
+        let compare = 0;
+        for (let i = data.length - 1; i >= 1; i--) {
+            for (let j = 0; j < i; j++) {
+                compare++;
+                if (data[j] > data[j + 1]) {
+                    let tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                }
+            }
+        }
+        console.log(`Tri à bulle: ${compare} comparaisons`);
+        return data;
+    };
+    bubbleSort(arr);
+    console.log(arr);
 });
