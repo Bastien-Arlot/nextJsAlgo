b89457cca2f4cefb5429495fe98c961c0f02445c
Bastien Arlot <arlot.b@gmail.com>
2022-04-20 16:36:48 +0200

    add merge and execution test

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const fs = require('fs');
+let arr;
 
 const fileName = process.argv[2];
+if(!fileName) {
+    console.log("Aucun fichier de données");
+}
 
-// Méthode asynchrone
-fs.readFile(fileName, 'utf8', (error, data) => {
-    if (error) {
-        console.error(error.message);
-        return ;
-    }
+try {
+    arr = fs.readFileSync(process.argv[2], "utf8");
+} catch (error) {
+    console.log("Le fichier renseigné déclanche les erreurs suivantes :");
+    console.error(error);
+    return;
+}
+arr = arr.split(" ").map((elem) => (!isNaN(Number(elem)) ? Number(elem) : null)).filter(Boolean);
 
-    const arr = data.split(" ").map(num => Number(num));
-
+if (arr.length < 1) {
+    console.log("Aucun nombre dans le fichier de données renseigné");
+    return;
+};
+console.log(arr);
     //BubbleSort
     const bubbleSort = (data) => {
         let compare = 0;
@@ -100,4 +109,42 @@ arrDuplicated = [...arr];
 
     arrDuplicated = [...arr];
     console.log(quickSort(arrDuplicated));
-});
+
+    // Merge sort
+    const mergeSort = (data) => {
+        let compare = 0;
+        const merge = (left, right) => {
+            let result = [];
+
+            while (left.length && right.length) {
+                if (left[0] <= right[0]) {
+                    result.push(left.shift());
+                } else {
+                    result.push(right.shift());
+                }
+            }
+            while (left.length) {
+                result.push(left.shift());
+            }
+            while (right.length) {
+                result.push(right.shift());
+            }
+            return result;
+        };
+
+        const sort = (data) => {
+            compare++;
+            if (data.length < 2) {
+                return data;
+            }
+            let middle = Math.floor(data.length / 2);
+            let left = data.slice(0, middle);
+            let right = data.slice(middle, data.length);
+            return merge(sort(left), sort(right));
+        };
+        data = sort(data);
+        console.log(`Tri fusion : ${compare} comparaisons`);
+        return data;
+    };
+    arrDuplicated = [...arr];
+    console.log(mergeSort(arrDuplicated));
