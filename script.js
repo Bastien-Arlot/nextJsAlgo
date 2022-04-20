86b22c5e91a808654486e81f386548e7e8893014
Bastien Arlot <arlot.b@gmail.com>
2022-04-20 15:58:11 +0200

    sort by insertion

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ fs.readFile(fileName, 'utf8', (error, data) => {
     }
     console.log(data);
     const arr = data.split(" ").map(num => Number(num));
+
+    //BubbleSort
     const bubbleSort = (data) => {
         let compare = 0;
         for (let i = data.length - 1; i >= 1; i--) {
@@ -25,6 +27,25 @@ fs.readFile(fileName, 'utf8', (error, data) => {
         console.log(`Tri à bulle: ${compare} comparaisons`);
         return data;
     };
-    bubbleSort(arr);
-    console.log(arr);
+
+
+
+    //Insertion Sort Algorithmns
+    const insertionSort = (data) => {
+        let compare = 0;
+        for (let i = 1; i < data.length; i++) {
+            let tmp = data[i];
+            let j = i;
+            while (j > 0 && data[j - 1] > tmp) {
+                compare++;
+                data[j] = data[j - 1];
+                j = j - 1;
+            }
+            data[j] = tmp;
+        }
+        console.log(`Tri par insertion: ${compare} comparaisons`);
+        return data;
+    };
+    console.log(insertionSort(arr));
+
 });
